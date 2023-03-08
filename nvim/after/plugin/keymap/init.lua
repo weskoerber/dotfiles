4ea0af4af8a2f681c069cda0ca8ab7cff3c04929
Wes Koerber <wes@weskoerber.com>
2023-03-08 13:30:57 -0500

    chore: window moving and navigation keybind changes

--- nvim/after/plugin/keymap/init.lua
+++ nvim/after/plugin/keymap/init.lua
@@ -1,12 +1,12 @@
 local Remap = require('weskoerber.keymap')
 local nnoremap = Remap.nnoremap
 
-nnoremap('<leader>ttc', ':tabnew<CR>')
-nnoremap('<leader>ttx', ':tabclose<CR>')
-nnoremap('<leader>ttn', ':tabnex<CR>')
-nnoremap('<leader>ttp', ':tabprevious<CR>')
+nnoremap('<C-n>', ':tabnew<CR>')
+nnoremap('<C-w>', ':tabclose<CR>')
+nnoremap('<C-]>', ':tabnex<CR>')
+nnoremap('<C-[>', ':tabprevious<CR>')
 
-nnoremap('<leader>j', ':winc j<CR>')
-nnoremap('<leader>k', ':winc k<CR>')
-nnoremap('<leader>h', ':winc h<CR>')
-nnoremap('<leader>l', ':winc l<CR>')
+nnoremap('<C-j>', ':winc j<CR>')
+nnoremap('<C-k>', ':winc k<CR>')
+nnoremap('<C-h>', ':winc h<CR>')
+nnoremap('<C-l>', ':winc l<CR>')
