3355e587e36897a00ac59ae3c0872800fc056e60
Wes Koerber <wes@weskoerber.com>
2023-05-06 21:13:14 -0400

    feat: add c debug config

--- nvim/lua/weskoerber/nvim-dap.lua
+++ nvim/lua/weskoerber/nvim-dap.lua
@@ -1,5 +1,41 @@
 local dap = require('dap')
 
+-- C
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = os.getenv("HOME") .. '/.local/share/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+    args = {
+        "main.db"
+    },
+  },
+  -- {
+  --   name = 'Attach to gdbserver :1234',
+  --   type = 'cppdbg',
+  --   request = 'launch',
+  --   MIMode = 'gdb',
+  --   miDebuggerServerAddress = 'localhost:1234',
+  --   miDebuggerPath = '/usr/bin/gdb',
+  --   cwd = '${workspaceFolder}',
+  --   program = function()
+  --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --   end,
+  -- },
+}
+
+-- C#
 dap.adapters.coreclr = {
     type = 'executable',
     command = os.getenv("HOME") .. '/.local/share/netcoredbg/netcoredbg',
@@ -16,5 +52,5 @@ dap.configurations.cs = {
         program = function()
             return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/', 'file')
         end,
-    }
+    },
 }
