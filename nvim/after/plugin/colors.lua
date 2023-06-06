a20f1fdf20bdccec74c15a6fba8c8e640283482f
Wes Koerber <wes@weskoerber.com>
2023-06-05 22:12:45 -0400

    chore: change colorscheme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,5 +1,14 @@
-vim.g.gruvbox_contrast_dark = 'soft'
-vim.g.gruvbox_invert_selection = '0'
-vim.g.gruvbox_italic = '1'
+-- vim.g.gruvbox_contrast_dark = 'soft'
+-- vim.g.gruvbox_invert_selection = '0'
+-- vim.g.gruvbox_italic = '1'
+-- vim.cmd('colorscheme gruvbox')
 
-vim.cmd('colorscheme gruvbox')
+-- require('rose-pine').setup({
+-- })
+--
+-- vim.cmd('colorscheme rose-pine')
+
+require('catppuccin').setup({
+    flavour = 'macchiato',
+})
+vim.cmd('colorscheme catppuccin')
