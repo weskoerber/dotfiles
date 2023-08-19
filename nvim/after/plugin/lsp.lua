6fc9c2d1be6931a703ac0a193dba50d42ea0825a
Wes Koerber <Wes Koerber wes@weskoerber.com>
2023-08-19 01:27:52 -0400

    chore: update keybinds

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -98,6 +98,7 @@ local default_keymap = function(bufnr, remap)
   vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set('n', '[u', function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
 end
 
 lsp.on_attach(function(_, bufnr)
--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,4 +1,4 @@
 local trouble = require('trouble')
 
-vim.keymap.set('n', '<leader>vca', function() trouble.open('workspace_diagnostics') end)
-vim.keymap.set('n', '<leader>vcd', function() trouble.open('document_diagnostics') end)
+vim.keymap.set('n', '<leader>dow', function() trouble.open('workspace_diagnostics') end)
+vim.keymap.set('n', '<leader>dod', function() trouble.open('document_diagnostics') end)
