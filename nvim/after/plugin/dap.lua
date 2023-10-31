1c2b6562ecc788901e214cfe9a4d8607b2dca2e9
Wes Koerber <wes@weskoerber.com>
2023-10-31 15:56:25 -0400

    chore: update dap

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -4,67 +4,67 @@ require('dap.ext.vscode').json_decode = require('json5').parse
 
 -- C
 dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = os.getenv("HOME") .. '/.local/share/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+    id = 'cppdbg',
+    type = 'executable',
+    command = os.getenv("HOME") .. '/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
 }
 
 dap.configurations.c = {
-  {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopAtEntry = true,
-    args = {
-        "main.db"
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to C executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+        args = {
+        },
     },
-  },
-  -- {
-  --   name = 'Attach to gdbserver :1234',
-  --   type = 'cppdbg',
-  --   request = 'launch',
-  --   MIMode = 'gdb',
-  --   miDebuggerServerAddress = 'localhost:1234',
-  --   miDebuggerPath = '/usr/bin/gdb',
-  --   cwd = '${workspaceFolder}',
-  --   program = function()
-  --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-  --   end,
-  -- },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+    },
 }
 
 dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopAtEntry = true,
-    args = {
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to C++ executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+        args = {
+        },
     },
-  },
-  -- {
-  --   name = 'Attach to gdbserver :1234',
-  --   type = 'cppdbg',
-  --   request = 'launch',
-  --   MIMode = 'gdb',
-  --   miDebuggerServerAddress = 'localhost:1234',
-  --   miDebuggerPath = '/usr/bin/gdb',
-  --   cwd = '${workspaceFolder}',
-  --   program = function()
-  --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-  --   end,
-  -- },
+
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+    },
 }
 
--- C#
+-- -- C#
 dap.adapters.coreclr = {
     type = 'executable',
     command = os.getenv("HOME") .. '/.local/share/netcoredbg/netcoredbg',
@@ -85,9 +85,9 @@ dap.configurations.cs = {
 }
 
 dap.adapters.php = {
-  type = 'executable',
-  command = 'node',
-  args = { '/usr/lib/node_modules/php-debug/out/phpDebug.js' },
+    type = 'executable',
+    command = 'node',
+    args = { '/usr/lib/node_modules/php-debug/out/phpDebug.js' },
 }
 
 vim.keymap.set('n', '<F5>', function() dap.continue() end)
