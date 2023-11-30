983fb4973854ea4983e98b9f1dfc3ff41994a39e
Wes Koerber <wes@weskoerber.com>
2023-11-30 16:27:02 -0500

    fix: call setup on nvim-surround

--- nvim/lua/weskoerber/lazy.lua
+++ nvim/lua/weskoerber/lazy.lua
@@ -73,6 +73,7 @@ require('lazy').setup({
         "kylechui/nvim-surround",
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
+        config = function() require('nvim-surround').setup() end
     },
     {
         'glepnir/dashboard-nvim',
