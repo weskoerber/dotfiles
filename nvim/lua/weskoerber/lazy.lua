be87018f6cdc5ef60ae1ac5659297628d1202993
Wes Koerber <wes@weskoerber.com>
2023-11-30 15:58:55 -0500

    chore: add barbar

--- nvim/lua/weskoerber/lazy.lua
+++ nvim/lua/weskoerber/lazy.lua
@@ -81,6 +81,21 @@ require('lazy').setup({
     },
     { 'kyazdani42/nvim-tree.lua' },
     { 'nvim-lualine/lualine.nvim' },
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
     { 'akinsho/toggleterm.nvim' },
 
     -- Git
