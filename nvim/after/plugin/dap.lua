41659bbe3f72bc5a8296ff8510a60c80dc35e0dc
Wes Koerber <wes@weskoerber.com>
2024-01-03 15:52:30 -0500

    fix: dap step into keybind

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -93,5 +93,5 @@ dap.adapters.php = {
 vim.keymap.set('n', '<F5>', function() dap.continue() end)
 vim.keymap.set('n', '<F8>', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-vim.keymap.set('n', '<F34>', function() dap.step_into() end)
+vim.keymap.set('n', '<F11>', function() dap.step_into() end)
 vim.keymap.set('n', '<F12>', function() dap.step_out() end)
