944739d42bb5c032708b1ce9c50b0088fc76c428
Wes Koerber <Wes Koerber wes@weskoerber.com>
2024-02-12 22:54:55 -0500

    fix: layout issues with long completion signature
    see https://github.com/hrsh7th/nvim-cmp/issues/1812

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -105,6 +105,9 @@ lsp.set_sign_icons({
 
 vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { strikethrough = true })
 
+-- Limit completions to 8 items at a time
+vim.opt.pumheight = 8;
+
 local cmp = require('cmp')
 cmp.setup({
   completion = {
@@ -113,15 +116,16 @@ cmp.setup({
   formatting = {
     format = require('lspkind').cmp_format({
       mode = 'symbol_text',  -- show only symbol annotations
-      maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      maxwidth = 30,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
       -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
       before = function(entry, vim_item)
+        vim_item.menu = string.sub(vim_item.menu or '', 0, 50) .. '...'
         return vim_item
       end
-    })
+    }),
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-k>'] = cmp.mapping.scroll_docs(-4),
