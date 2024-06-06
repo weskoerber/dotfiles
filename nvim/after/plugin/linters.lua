aff637e112cd678191591991586bd48207ed65e5
Wes Koerber <wes@weskoerber.com>
2024-06-06 02:59:47 -0400

    chore: php linter and formatter

--- nvim/after/plugin/linters.lua
+++ nvim/after/plugin/linters.lua
@@ -1,15 +1,15 @@
 local lint = require('lint')
 
 -- LINTERS
--- lint.linters_by_ft = {
---     php = { 'phpcs' },
--- }
---
--- vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufWritePost" }, {
---     callback = function()
---         lint.try_lint()
---     end,
--- })
+lint.linters_by_ft = {
+    php = { 'phpcs' },
+}
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufRead", "BufWritePost" }, {
+    callback = function()
+        lint.try_lint()
+    end,
+})
 
 local lint_progress = function()
     local linters = lint.get_running()
