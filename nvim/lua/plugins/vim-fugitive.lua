e66b491d49f9930bf333a7d17177af4d19c61ae0
Wes Koerber <Wes Koerber wes@weskoerber.com>
2024-10-04 10:14:53 -0400

    chore: restore fugitive toggling

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,7 +1,24 @@
+local utils = require('utils')
+
+vim.api.nvim_create_user_command('FugitiveToggle', function()
+    local found_buf = false
+    for _, bufnr in pairs(vim.api.nvim_list_bufs()) do
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if vim.startswith(bufname, 'fugitive://') then
+            found_buf = true
+            vim.api.nvim_buf_delete(bufnr, {})
+        end
+    end
+
+    if found_buf == false then
+        vim.cmd([[Git]])
+    end
+end, {})
+
 return {
     'tpope/vim-fugitive',
     keys = {
-        {'<leader>gg', '<cmd>Git<cr>'},
+        {'<leader>gg', '<cmd>FugitiveToggle<cr>'},
     },
     config = function()
         vim.keymap.set('n', '<leader>gf', '<cmd>Git fetch<cr>')
