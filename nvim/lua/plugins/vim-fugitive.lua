b2e6d6690daf4b0ae6430840dd77d9a8a643918b
Wes Koerber <Wes Koerber wes@weskoerber.com>
2024-10-04 01:00:49 -0400

    chore(nvim): fugitive keys

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -3,4 +3,9 @@ return {
     keys = {
         {'<leader>gg', '<cmd>Git<cr>'},
     },
+    config = function()
+        vim.keymap.set('n', '<leader>gf', '<cmd>Git fetch<cr>')
+        vim.keymap.set('n', '<leader>gy', '<cmd>Git push --force<cr>') -- yeet
+        vim.keymap.set('n', '<leader>gY', '<cmd>Git push --force<cr>') -- YEET
+    end,
 }
