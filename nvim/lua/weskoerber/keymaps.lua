1d1d049ae6a28bb1e361f718a4ca1b2ca01dcecd
Wes Koerber <wes@weskoerber.com>
2024-12-05 14:45:08 -0500

    refactor(nvim): keymaps

--- nvim/lua/weskoerber/keymaps.lua
+++ nvim/lua/weskoerber/keymaps.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ';'
+
 -- splits
 vim.keymap.set('n', '<leader>gsv', function()
     vim.cmd.vsplit()
@@ -7,3 +10,9 @@ vim.keymap.set('n', '<leader>gsh', function()
     vim.cmd.split()
     vim.cmd.wincmd('j')
 end);
+
+-- Buffer navigation
+vim.keymap.set('n', '<M-j>', ':winc j<CR>')
+vim.keymap.set('n', '<M-k>', ':winc k<CR>')
+vim.keymap.set('n', '<M-h>', ':winc h<CR>')
+vim.keymap.set('n', '<M-l>', ':winc l<CR>')
--- nvim/plugin/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Buffer navigation
-vim.keymap.set('n', '<leader>wj', ':winc j<CR>')
-vim.keymap.set('n', '<leader>wk', ':winc k<CR>')
-vim.keymap.set('n', '<leader>wh', ':winc h<CR>')
-vim.keymap.set('n', '<leader>wl', ':winc l<CR>')
