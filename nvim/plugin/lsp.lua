40e5c9452eeadc0e2234f66f013c07cf362bf6fd
Wes Koerber <wes@weskoerber.com>
2025-10-11 05:54:13 -0400

    feat(nvim): native completions, additional keymaps

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -8,3 +8,22 @@ for name, config in pairs(configs) do
     vim.lsp.config(name, config)
     vim.lsp.enable(name)
 end
+
+vim.opt.completeopt = { 'menuone', 'noselect', 'popup', }
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        vim.g.maplocalleader = ','
+
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        assert(client ~= nil, 'must have valid client')
+
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            autotrigger = true,
+        })
+
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+        vim.keymap.set('n', '<localleader>d', vim.diagnostic.open_float)
+        vim.keymap.set('n', '<localleader>rn', vim.lsp.buf.rename)
+    end,
+})
