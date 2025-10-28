dd64789873d864aa1dc7a09aadcd8de109a8ea77
Wes Koerber <wes@weskoerber.com>
2025-10-28 13:49:01 -0400

    chore(nvim): add php_cs_fixer formatter

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -4,4 +4,13 @@ conform.setup({
         timeout_ms = 500,
         lsp_format = 'fallback',
     },
+    formatters_by_ft = {
+        php = { 'php_cs_fixer' },
+    },
+    formatters = {
+        php_cs_fixer = {
+            command = vim.fn.resolve(vim.fs.joinpath('vendor', 'bin', 'php-cs-fixer')),
+            args = { 'fix', '$FILENAME', '--quiet', '--config=.cs.php' },
+        },
+    },
 })
