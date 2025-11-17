ee36e65cf50428ca2e05d1b2c8af9c7b23043ab6
Wes Koerber <wes@weskoerber.com>
2025-11-17 14:49:31 -0500

    fix(nvim): trim trailing space autocmd not restoring cursor pos

--- nvim/lua/weskoerber/commands.lua
+++ nvim/lua/weskoerber/commands.lua
@@ -3,7 +3,12 @@ local M = {
         vim.api.nvim_create_autocmd('BufWritePre', {
             group = opts.write_group,
             pattern = '*',
-            command = '%s/\\s\\+$//e',
+            callback = function()
+                local win = vim.api.nvim_get_current_win()
+                local cur = vim.api.nvim_win_get_cursor(win)
+                vim.cmd('%s/\\s\\+$//e')
+                vim.api.nvim_win_set_cursor(win, cur)
+            end,
         })
     end
 }
