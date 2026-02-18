48e8a39d565e58793f134be1d27b83ae43ffad7e
Wes Koerber <wes@weskoerber.com>
2026-02-17 23:42:18 -0500

    chore(nvim): treesitter breaking changes

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,16 +1,20 @@
-require('nvim-treesitter.config').setup({
-    ensure_installed = {
-        'c', 'cpp', 'c_sharp', 'go', 'rust',
-        'bash', 'javascript', 'lua', 'php', 'phpdoc', 'sql',
-        'git_rebase', 'gitattributes', 'gitcommit', 'gitignore', 'git_config',
-        'cmake', 'make',
-        'json', 'jsonc', 'markdown', 'markdown_inline', 'toml', 'yaml',
-        'comment', 'diff', 'http', 'vim', 'vimdoc',
-    },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'markdown' },
-    },
-    indent = { enable = true },
-    sync_install = false,
+require('nvim-treesitter').install({
+    'c', 'cpp', 'c_sharp', 'go', 'rust',
+    'bash', 'javascript', 'lua', 'php', 'phpdoc', 'sql',
+    'git_rebase', 'gitattributes', 'gitcommit', 'gitignore', 'git_config',
+    'cmake', 'make',
+    'json', 'markdown', 'markdown_inline', 'toml', 'yaml',
+    'comment', 'diff', 'http', 'vim', 'vimdoc',
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(args)
+        local bufnr = args.buf
+        local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+        if not ok or not parser then
+            return
+        end
+
+        vim.treesitter.start(bufnr)
+    end,
+});
