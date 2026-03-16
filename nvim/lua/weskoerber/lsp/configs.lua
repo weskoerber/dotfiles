f81de1acb5b2ecbdfe8f9e699afac412050d05c7
Wes Koerber <wes@weskoerber.com>
2026-03-16 14:31:09 -0400

    fix(lsp): omnisharp not starting

--- nvim/lua/weskoerber/lsp/configs.lua
+++ nvim/lua/weskoerber/lsp/configs.lua
@@ -1,3 +1,5 @@
+local util = require 'lspconfig.util'
+
 -- Save default config callbacks here
 -- https://github.com/neovim/neovim/issues/33577#issuecomment-2832231240
 local default_cbs = {
@@ -20,28 +22,88 @@ return {
         },
     },
     omnisharp = {
-        -- NOTE: For some reason, specifying the command doesn't work
-        -- cmd = {
-        --     vim.fn.resolve(
-        --         vim.fs.joinpath(vim.fn.stdpath('data'), 'mason', 'bin', 'OmniSharp')
-        --     ),
-        -- },
-        settings = {
-            FormattingOptions = {
-                EnabledEditorConfigSupport = true,
-                OrganizeImports = true,
+        cmd = {
+            vim.fn.resolve(
+                vim.fs.joinpath(vim.fn.stdpath('data'),
+                    'mason',
+                    'bin',
+                    'OmniSharp'
+                )
+            ),
+            '-z', -- https://github.com/OmniSharp/omnisharp-vscode/pull/4300
+            '--hostPID',
+            tostring(vim.fn.getpid()),
+            'DotNet:enablePackageRestore=false',
+            '--encoding',
+            'utf-8',
+            '--languageserver',
+        },
+        filetypes = { 'cs', 'vb' },
+        root_dir = function(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            on_dir(
+                util.root_pattern '*.slnx' (fname)
+                or util.root_pattern '*.sln' (fname)
+                or util.root_pattern '*.csproj' (fname)
+                or util.root_pattern 'omnisharp.json' (fname)
+                or util.root_pattern 'function.json' (fname)
+            )
+        end,
+        init_options = {},
+        capabilities = {
+            workspace = {
+                workspaceFolders = false, -- https://github.com/OmniSharp/omnisharp-roslyn/issues/909
             },
-            RoslynExtensionsOptions = {
-                enableDecompilationSupport = true,
-            }
         },
         on_attach = function(_)
+            vim.print('attaching omnisharp')
             local oe = require('omnisharp_extended')
             vim.keymap.set('n', 'gd', oe.lsp_definition)
             vim.keymap.set('n', 'gt', oe.lsp_type_definition)
             vim.keymap.set('n', 'gr', oe.lsp_references)
             vim.keymap.set('n', 'gi', oe.lsp_implementation)
         end,
+        settings = {
+            FormattingOptions = {
+                EnableEditorConfigSupport = true,
+                OrganizeImports = true,
+            },
+            MsBuild = {
+                -- If true, MSBuild project system will only load projects for files that
+                -- were opened in the editor. This setting is useful for big C# codebases
+                -- and allows for faster initialization of code navigation features only
+                -- for projects that are relevant to code that is being edited. With this
+                -- setting enabled OmniSharp may load fewer projects and may thus display
+                -- incomplete reference lists for symbols.
+                LoadProjectsOnDemand = nil,
+            },
+            RoslynExtensionsOptions = {
+                -- Enables support for roslyn analyzers, code fixes and rulesets.
+                EnableAnalyzersSupport = nil,
+                -- Enables support for showing unimported types and unimported extension
+                -- methods in completion lists. When committed, the appropriate using
+                -- directive will be added at the top of the current file. This option can
+                -- have a negative impact on initial completion responsiveness,
+                -- particularly for the first few completion sessions after opening a
+                -- solution.
+                EnableImportCompletion = true,
+                -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                -- true
+                AnalyzeOpenDocumentsOnly = nil,
+                -- Enables the possibility to see the code in external nuget dependencies
+                EnableDecompilationSupport = true,
+            },
+            RenameOptions = {
+                RenameInComments = nil,
+                RenameOverloads = nil,
+                RenameInStrings = nil,
+            },
+            Sdk = {
+                -- Specifies whether to include preview versions of the .NET SDK when
+                -- determining which version to use for project loading.
+                IncludePrereleases = true,
+            },
+        },
     },
     rust_analyzer = {
         settings = {
