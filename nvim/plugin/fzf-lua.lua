5399d3b47c53ebcce274a9ccb1f8e6c673adad80
Wes Koerber <wes@weskoerber.com>
2026-08-06 12:42:54 -0400

    chore(nvim): additional fzf-lua keybinds

--- nvim/plugin/fzf-lua.lua
+++ nvim/plugin/fzf-lua.lua
@@ -11,12 +11,16 @@ vim.keymap.set('n', '<leader>lf', fzf.lsp_document_symbols)
 vim.keymap.set('n', '<leader>lw', fzf.lsp_workspace_symbols)
 vim.keymap.set('n', '<leader>ld', fzf.lsp_document_diagnostics)
 vim.keymap.set('n', '<leader>lp', fzf.lsp_workspace_diagnostics)
+vim.keymap.set('n', '<leader>li', fzf.lsp_incoming_calls)
 
-vim.keymap.set('n', '<leader>fc', fzf.git_commits)
-vim.keymap.set('n', '<leader>fg', fzf.git_files)
+vim.keymap.set('n', '<leader>gb', fzf.git_branches)
+vim.keymap.set('n', '<leader>gc', fzf.git_commits)
+vim.keymap.set('n', '<leader>gf', fzf.git_files)
 
 vim.keymap.set('n', '<leader>fb', fzf.buffers)
 vim.keymap.set('n', '<leader>fj', fzf.marks)
+vim.keymap.set('n', '<leader>fw', fzf.grep_cWORD)
+vim.keymap.set('n', '<leader>ft', fzf.lgrep_curbuf)
 
 vim.keymap.set('n', '<leader>fh', fzf.helptags)
 vim.keymap.set('n', '<leader>fm', fzf.manpages)
